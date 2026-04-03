fcf6877fa86ccf6837cc528edbe79809c9479792
Bashar Umar <basharu83@gmail.com>
2026-04-03 12:14:19 +0100

    feat: implement custom login response and update routing for onboarding; add new database files

---
+++ app/Http/Responses/LoginResponse.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
+
+class LoginResponse implements LoginResponseContract
+{
+    /**
+     * Create an HTTP response that represents the object.
+     */
+    public function toResponse($request)
+    {
+        return $request->wantsJson()
+            ? response()->json(['two_factor' => false])
+            : redirect()->intended(route('onboarding.continue'));
+    }
+}
