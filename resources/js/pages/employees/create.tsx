fbf6ca376af4bb9cbea010d2c16cfcd78ec67042
Bashar Umar <basharu83@gmail.com>
2026-05-11 11:27:08 +0100

    feat(employee): add checkbox component to employee creation page

--- resources/js/pages/employees/create.tsx
+++ resources/js/pages/employees/create.tsx
@@ -11,6 +11,7 @@ import {
     CardHeader,
     CardTitle,
 } from '@/components/ui/card';
+import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AppLayout from '@/layouts/app-layout';
